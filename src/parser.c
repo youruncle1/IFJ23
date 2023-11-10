78f4913dc0de9d7a5b01904252d94f9bef94bdd5
xrusna08 <xrusna08@vutbr.cz>
2023-11-10 22:37:10 +0100

    Added functionality to move through tokens, improved upon code and added the rest of nonterminal functions. Also found some mistakes in LL. Co-authored-by: Roman Poliačik <youruncle1@users.noreply.github.com>

--- src/parser.c
+++ src/parser.c
@@ -14,10 +14,34 @@ authors: xpolia05
 #include "error.h"
 #include "parser.h"
 
-/*Global variable Current Token a Scanner*/
+/*Global variable Current Token and Scanner*/
 token_t CurrentToken;
 scanner_t scanner;
 
+
+bool Check_Token_Type ( tk_type_t type )
+{
+        return CurrentToken.type == type;
+}
+
+bool Check_Next_Token_Type ( tk_type_t type, char* errMsg )
+{
+    Next_Token();
+    // return CurrentToken.type == type;
+    if ( CurrentToken.type == type )
+    {
+        return true;
+    }
+    else
+    {
+        handle_error(SYNTAX_ERROR, CurrentToken.line, errMsg);
+    }
+}
+
+void Next_Token()
+{
+    CurrentToken = get_token(&scanner);
+}
 
 int Program()
 {
@@ -28,13 +52,13 @@ int Program()
 
 int Block_Contents()
 {
-    if ( CurrentToken.type == TK_KW_VAR || CurrentToken.type == TK_KW_LET || CurrentToken.type == TK_KW_FUNC )
+    if ( Check_Token_Type( TK_KW_VAR ) || Check_Token_Type( TK_KW_LET ) || Check_Token_Type( TK_KW_FUNC ) )//CurrentToken.type == TK_KW_VAR || CurrentToken.type == TK_KW_LET || CurrentToken.type == TK_KW_FUNC )
     {
         Block_Content();
 
         Block_Contents();
     }
-    else if ( CurrentToken.type == TK_EOF )
+    else if ( Check_Token_Type( TK_EOF ) )
     {
         return ERROR_FREE;
     }
@@ -51,9 +75,20 @@ int Block_Content()
     {
         case TK_KW_VAR:
         case TK_KW_LET:
-            Def_Var();
+            Next_Token();
+            if ( Check_Token_Type( TK_IDENTIFIER ) )
+            {
+                Type_Opt();
+                Def_Var();
+            }
+            else
+            {
+                handle_error(SYNTAX_ERROR, CurrentToken.line, "Expected identifier");
+                return ERROR_OCCURED;
+            }
             break;
         case TK_KW_FUNC:
+            Next_Token();
             Function_Definition();
             break;
         default:
@@ -62,170 +97,232 @@ int Block_Content()
 
 int Def_Var()
 {
-    CurrentToken = get_token(&scanner);
-    if ( CurrentToken.type != TK_IDENTIFIER )
+    Next_Token();
+    if ( Check_Token_Type ( TK_ASSIGN ) )
     {
-        handle_error(SYNTAX_ERROR, CurrentToken.line, "Expected identifier");
-        printf(stderr, "ERROR: Expected identifier, got %s on line %d\n", token_type_to_string(CurrentToken.type), CurrentToken.line);
-        return ERROR_OCCURED;
+        Next_Token();
+        Def_Value();
+        return ERROR_FREE;
     }
-
-    CurrentToken = get_token(&scanner);
-    
-    Type_Opt();
-    
-    Def_Var();
 }
 
 int Function_Definition()
 {
-    CurrentToken = get_token(&scanner);
-
-    if ( CurrentToken.type != TK_IDENTIFIER )
-    {
-        handle_error(SYNTAX_ERROR, CurrentToken.line, "Expected identifier");
-        return ERROR_OCCURED;
-    }
-
-    CurrentToken = get_token(&scanner);
-    if ( CurrentToken.type != TK_LPAR )
-    {
-        handle_error(SYNTAX_ERROR, CurrentToken.line, "Expected '('");
-        return ERROR_OCCURED;
-    }
-
-    CurrentToken = get_token(&scanner);
-
+    Check_Next_Token_Type( TK_IDENTIFIER , "Expected identifier" );
+    Check_Next_Token_Type( TK_LPAR , "Expected '('" );
+    Next_Token();
     Parameter_List();
-
-    if ( CurrentToken.type != TK_RPAR )
+    Check_Next_Token_Type( TK_RPAR, "Expected ')'" );
+    Next_Token();
+    if ( Check_Token_Type( TK_ARROW ) )
     {
-        handle_error(SYNTAX_ERROR, CurrentToken.line, "Expected ')'");
-        return ERROR_OCCURED;
+        Next_Token();
+        Return_Type();
     }
-
-    CurrentToken = get_token(&scanner);
-    if ( CurrentToken.type != TK_ARROW )
-    {
-        handle_error(SYNTAX_ERROR, CurrentToken.line, "Expected '->'");
-        return ERROR_OCCURED;
-    }
-
-    CurrentToken = get_token(&scanner);
-    
-    Return_Type();
-
     Function_Body();
-
+    return ERROR_FREE;
 }
 
 int Type_Opt()
 {
-    if ( CurrentToken.type == TK_COLON )
+    Next_Token();
+    if ( Check_token_Type( TK_COLON ) )
     {
-        CurrentToken = get_token(&scanner);
+        Next_Token();
         Type();
     }
     return ERROR_FREE;
 }
 
-/*TODO zistit ake typy tam su a opravit funkciu Type()!*/
-
 int Type()
 {
     switch(CurrentToken.type)
     {
-        case TK_INT:
-        case TK_DOUBLE:
-        case TK_STRING:
-            CurrentToken = get_token(&scanner);
+        case TK_KW_INT:
+        case TK_KW_DOUBLE:
+        case TK_KW_STRING:
+        case TK_KW_INT_OPT:
+        case TK_KW_DOUBLE_OPT:
+        case TK_KW_STRING_OPT:
             break;
         default:
             handle_error(SYNTAX_ERROR, CurrentToken.line, "Expected type");
             return ERROR_OCCURED;
     }
+    return ERROR_FREE;
 }
 
 int Return_Type()
 {
-    switch(CurrentToken.type)
-    {
-        case TK_INT:
-        case TK_DOUBLE:
-        case TK_STRING:
-        case TK_KW_NIL:
-            CurrentToken = get_token(&scanner);
-            return ERROR_FREE;
-        default:
-            handle_error(SYNTAX_ERROR, CurrentToken.line, "Expected type or 'NIL'");
-            return ERROR_OCCURED;
-    }
-
+    Next_Token();
+    Type();
 }
 
 int Parameter_List()
 {
-    if ( CurrentToken.type == TK_IDENTIFIER )
-    {
-        Parameter_Definition();
-        Parameter_List_Tail();
-    }
-    else if ( CurrentToken.type == TK_RPAR )
-    {
-        return ERROR_FREE;
-    }
-    else
-    {
-        handle_error(SYNTAX_ERROR, CurrentToken.line, "Expected identifier or ')'");
-        return ERROR_OCCURED;
-    }
+    Next_Token();
+    Parameter_Definition();
+    Next_Token();
+    Parameter_List_Tail();
 }
 
 int Parameter_List_Tail()
-{
-    if ( CurrentToken.type == TK_COMMA )
+{   
+    if ( Check_Token_Type( TK_COMMA ) )
     {
-        CurrentToken = get_token(&scanner);
+        Next_Token();
         Parameter_Definition();
+        Next_Token();
         Parameter_List_Tail();
     }
-    else if ( CurrentToken.type == TK_RPAR )
-    {
-        return ERROR_FREE;
-    }
-    else
-    {
-        handle_error(SYNTAX_ERROR, CurrentToken.line, "Expected ',' or ')'");
-        return ERROR_OCCURED;
-    }
+    return ERROR_FREE;
 }
 
 int Parameter_Definition()
 {
-    if ( CurrentToken.type == TK_IDENTIFIER || CurrentToken.type == TK_UNDERSCORE)
+    if ( Check_Token_Type ( TK_IDENTIFIER ) || Check_Token_Type ( TK_UNDERSCORE ) )
     {
-        CurrentToken = get_token(&scanner);
-        if ( CurrentToken.type == TK_COLON )
-        {
-            CurrentToken = get_token(&scanner);
-            Type();
-        }
-        else
-        {
-            handle_error(SYNTAX_ERROR, CurrentToken.line, "Expected ':'");
-            return ERROR_OCCURED;
-        }
+        Check_Next_Token_Type( TK_COLON, "Expected ':'" );
+        Next_Token();
+        Type();
+        return ERROR_FREE;
     } 
     else
     {
         handle_error(SYNTAX_ERROR, CurrentToken.line, "Expected identifier or '_'");
         return ERROR_OCCURED;
     }
-    return ERROR_FREE;
+
+}
+
+int Def_Value()
+{
+    /*TODO: Zistit ci mam zavolat expression alebo function call*/
+    Next_Token();
+    Function_Call();
 }
 
 int Function_Body()
 {
+    if ( Check_Token_Type( TK_LBRACE ) )
+    {
+        Next_Token();
+        Statement_List();
+        Check_Next_Token_Type( TK_RBRACE, "Expected '}'" );
+        return ERROR_FREE;
+    }
+    else
+    {
+        handle_error(SYNTAX_ERROR, CurrentToken.line, "Expected '{'");
+        return ERROR_OCCURED;
+    }
+}
 
+int Statement_List()
+{
+    Statement();
+    Next_Token();
+    Statement_List();
+}
+
+int Statement()
+{
+    switch( CurrentToken.type )
+    {
+        case TK_KW_IF:
+            Next_Token();
+            if ( Check_Token_Type( TK_KW_LET ) )
+            {
+                Check_Next_Token_Type( TK_IDENTIFIER, "Expected identifier" );
+                Check_Next_Token_Type( TK_LBRACE, "Expected '{'" );
+                Next_Token();
+                Statement_List();
+                Check_Next_Token_Type( TK_RBRACE, "Expected '}'" );
+                Check_Next_Token_Type( TK_KW_ELSE, "Expected 'else'" );
+                Check_Next_Token_Type( TK_LBRACE, "Expected '{'" );
+                Next_Token();
+                Statement_List();
+                Check_Next_Token_Type( TK_RBRACE, "Expected '}'" );
+            }
+            else
+            {
+                //TODO: evaluate expression
+                Check_Next_Token_Type( TK_LBRACE, "Expected '{'" );
+                Next_Token();
+                Statement_List();
+                Check_Next_Token_Type( TK_RBRACE, "Expected '}'" );
+                Check_Next_Token_Type( TK_KW_ELSE, "Expected 'else'" );
+                Check_Next_Token_Type( TK_LBRACE, "Expected '{'" );
+                Next_Token();
+                Statement_List();
+                Check_Next_Token_Type( TK_RBRACE, "Expected '}'" );
+            }
+            break;
+        case TK_KW_WHILE:
+            //TODO: evaluate expression
+            Check_Next_Token_Type( TK_LBRACE, "Expected '{'" );
+            Next_Token();
+            Statement_List();
+            Check_Next_Token_Type( TK_RBRACE, "Expected '}'" );
+            break;
+        case TK_IDENTIFIER:
+            Check_Next_Token_Type( TK_ASSIGN, "Expected '='" );
+            Next_Token();
+            Function_Call();
+            break;
+        case TK_KW_VAR:
+        case TK_KW_LET:
+            Check_Next_Token_Type( TK_IDENTIFIER, "Expected identifier" );
+            Next_Token();
+            Type_Opt();
+            Next_Token();
+            Def_Var();
+            break;
+        case TK_KW_RETURN:
+            //TODO: Dorobit gramatiku na return (Funckia moze byt void a prikaz return tam vobec nemusi byt)
+            break;
+    }
+}
+
+int Function_Call()
+{
+    if ( Check_Token_Type( TK_IDENTIFIER ) )
+    {
+        Check_Next_Token_Type( TK_LPAR, "Expected '('");
+        Next_Token();
+        Input_Parameters();
+        Check_Next_Token_Type( TK_RPAR, "Expected ')'");
+    }
+    else
+    {
+        handle_error(SYNTAX_ERROR, CurrentToken.line, "Expected the name of defined function");
+        return ERROR_OCCURED;
+    }
+    return ERROR_FREE;
+}
+
+int Input_Parameters()
+{
+    Next_Token();
+    Term();
+    Next_Token();
+    Input_Parameters_Tail();
+}
+
+int Input_Parameter_Tail()
+{
+    if ( Check_Token_Type( TK_COMMA ) )
+    {
+        Next_Token();
+        Term();
+        Next_Token();
+        Input_Parameters_Tail();
+    }
+    return ERROR_FREE;
+}
+
+int Term()
+{
+    //TODO: zistit, ci je token ID alebo ci to je expression
 }
 
--- src/parser.h
+++ src/parser.h
@@ -6,43 +6,133 @@
 #define ERROR_OCCURED 1
 #define ERROR_FREE 0
 
+
+/**
+ * @brief Function for checking the type of the token
+ * @param type expected type of the token
+ * @return bool
+ */
+bool Check_Token_Type ( tk_type_t type );
+
+/**
+ * @brief Function for checking the type of the next token
+ * @param type expected type of the token
+ * @param errMsg error message
+ * @return bool
+ */
+bool Check_Next_Token_Type ( tk_type_t type, char* errMsg );
+
+/**
+ * @brief Function for parsing the whole program
+ * @return int
+ */
 int Program();
 
+/**
+ * @brief Function for parsing the block contents
+ * @return int
+ */
 int Block_Contents();
 
+/**
+ * @brief Function for parsing the block content
+ * @return int
+*/
 int Block_Content();
 
+/**
+ * @brief Funcion gets the type of the variable
+ * @return int
+*/
 int Type_Opt();
 
+/**
+ * @brief Function for parsing the variable definition
+ * @return int
+*/
 int Def_Var();
 
+/**
+ * @brief Function for parsing the variable value
+ * @return int
+*/
 int Def_Value();
 
+/**
+ * @brief Function to get the type of token
+*/
 int Type();
 
+/**
+ * @brief Function for parsing the function definition
+ * @return int
+*/
 int Function_Definition();
 
+/**
+ * @brief Function for parsing the function parameters
+ * @return int
+*/
 int Parameter_List();
 
+/**
+ * @brief Function for parsing the function parameters
+ * @return int
+*/
 int Parameter_List_Tail();
 
+/**
+ * @brief Function for parsing the function parameters
+ * @return int
+*/
 int Parameter_Definition();
 
+/**
+ * @brief Function to get the return type of the function
+ * @return int
+*/
 int Return_Type();
 
+/**
+ * @brief Function for parsing the function body
+ * @return int
+*/
 int Function_Body();
 
+/**
+ * @brief Function for parsing the statement list
+ * @return int
+*/
 int Statement_List();
 
+/**
+ * @brief Function for parsing individual statements
+ * @return int
+*/
 int Statement();
 
+/**
+ * @brief Function for parsing function call
+ * @return int
+*/
 int Function_Call();
 
+/**
+ * @brief Function for parsing the input parameters of function call
+ * @return int
+*/
 int Input_Parameters();
 
+/**
+ * @brief Helper function for parsing the input parameters of function call
+ * @return int
+*/
 int Input_Parameter_Tail();
 
+/**
+ * @brief Helper function to parse the input parameters of function call
+ * @return int
+*/
 int Term();
 
-int Expression();
 
